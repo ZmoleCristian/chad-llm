487a0f5861b2648521746c5185265a5cb6a2421c
N1ptic <criste.ioan92@yahoo.com>
2024-07-24 13:32:39 +0300

    added extra features, first commit

--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,80 @@
+mod commands;
 mod data;
 mod openai;
+mod response;
 
-use bat::PrettyPrinter;
 use clipboard::{ClipboardContext, ClipboardProvider};
+use commands::{handle_command, is_command};
 use data::MyCompletion;
 use dialoguer::{theme::ColorfulTheme, BasicHistory, Input};
 use openai::send_request;
-use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::sync::Arc;
 use tokio::runtime::Runtime;
-use tokio_stream::StreamExt;
+use tokio::sync::Mutex;
 
-fn is_command(input: &str) -> bool {
-    input.starts_with('/') && !input.strip_prefix('/').unwrap().contains(' ')
-}
-
-fn command_handler(cmd: String) {
-    match cmd.split('/').collect::<Vec<&str>>()[1] {
-        "exit" => {
-            std::process::exit(0);
-        }
-        "clear" => {
-            println!("\x1B[2J\x1B[1;1H");
-        }
-        "paste" => {
-            let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
-            let content = clipboard.get_contents().unwrap();
-            PrettyPrinter::new()
-                .input_from_bytes(content.as_bytes())
-                .language("rust")
-                .print()
-                .unwrap();
-        }
-        _ => {
-            println!("Unknown command: {}", cmd);
-        }
-    }
-}
+type SharedContext = Arc<Mutex<Vec<serde_json::Value>>>;
 
 fn main() {
-    //Mite-ma frumos ca vei reformata acest spaghetti main
+    let context = Arc::new(Mutex::new(Vec::new()));
     let mut history = BasicHistory::new().max_entries(99).no_duplicates(false);
     let rt = Runtime::new().unwrap();
     let completion = MyCompletion::default();
+
     loop {
-        if let Ok(input) = Input::<String>::with_theme(&ColorfulTheme::default())
-            .with_prompt("TragDate")
+        let mut code_blocks: Vec<String> = Vec::new(); // Reset code_blocks for each interaction
+
+        let mut input = Input::<String>::with_theme(&ColorfulTheme::default())
+            .with_prompt("N1ptic") // Add newline before prompt
             .completion_with(&completion)
             .history_with(&mut history)
             .interact_text()
-        {
-            if input.starts_with('/') && is_command(&input) {
-                command_handler(input);
-            } else {
-                let mut accumulate = false;
-                let mut accumulator: Vec<String> = Vec::new();
-                let mut end_delimiter_buffer = String::new();
-                let response_stream = rt.block_on(send_request(&input));
-                match response_stream {
-                    Ok(mut stream) => {
-                        rt.block_on(async {
-                            while let Some(chunk) = stream.next().await {
-                                match chunk {
-                                    Ok(content) => {
-                                        if content.trim() == "```" {
-                                            accumulate = !accumulate;
-                                            if !accumulate {
-                                                // End of code block, print accumulated code
-                                                let code = accumulator.join("");
-                                                PrettyPrinter::new()
-                                                    .input_from_bytes(code.as_bytes())
-                                                    .language("rust")
-                                                    .print()
-                                                    .unwrap();
-                                                accumulator.clear();
-                                                end_delimiter_buffer.clear();
-                                                append_to_chat(&code);
-                                            }
-                                        } else if accumulate {
-                                            end_delimiter_buffer.push_str(&content.trim());
-                                            if end_delimiter_buffer.ends_with("```") {
-                                                // End of code block, print accumulated code
-                                                let code = accumulator.join("");
-                                                let trimmed: String =
-                                                    code.chars().filter(|&c| c != '`').collect();
+            .unwrap();
 
-                                                PrettyPrinter::new()
-                                                    .input_from_bytes(trimmed.as_bytes())
-                                                    .language("rust")
-                                                    .print()
-                                                    .unwrap();
-                                                accumulator.clear();
-                                                accumulate = false;
-                                                end_delimiter_buffer.clear();
-                                                append_to_chat(&trimmed);
-                                            } else {
-                                                accumulator.push(content.clone());
-                                            }
-                                        } else {
-                                            let trimmed: String =
-                                                content.chars().filter(|&c| c != '`').collect();
-                                            print!("{}", trimmed);
-                                            append_to_chat(&trimmed);
-                                        }
-                                    }
-                                    Err(err) => eprintln!("Error: {}", err),
-                                }
-                            }
-                        });
-                    }
-                    Err(err) => eprintln!("Error: {}", err),
-                }
-                println!();
+        // Check if the input is a command
+        if is_command(&input) {
+            handle_command(&input, &code_blocks);
+            if input.trim() == "/paste" {
+                // If the command was /paste, append the clipboard content to the input
+                let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
+                let paste_content = clipboard.get_contents().unwrap();
+                print!("\n{}", paste_content); // Print the clipboard content
+                std::io::stdout().flush().unwrap();
+
+                let additional_input = Input::<String>::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Add additional details")
+                    .interact_text()
+                    .unwrap();
+
+                input.push_str(&paste_content);
+                input.push_str(&additional_input);
             }
         }
+
+        if !is_command(&input) {
+            let response_stream = rt.block_on(send_request(&input, Arc::clone(&context)));
+            match response_stream {
+                Ok(stream) => {
+                    rt.block_on(response::process_response(
+                        Box::pin(stream),
+                        &mut code_blocks,
+                    ))
+                    .unwrap();
+                }
+                Err(err) => eprintln!("Request failed: {}", err),
+            }
+
+            if !code_blocks.is_empty() {
+                if let Ok(command_input) = Input::<String>::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Enter command")
+                    .interact_text()
+                {
+                    handle_command(&command_input, &code_blocks);
+                }
+            }
+        }
+        println!(); // Ensure new line after each interaction
+        std::io::stdout().flush().unwrap(); // Ensure stdout is flushed after each interaction
     }
 }
-
-fn append_to_chat(content: &str) {
-    let mut file = OpenOptions::new().append(true).open("chat.txt").unwrap();
-    writeln!(file, "{}", content).unwrap();
-}
-
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//paddinggggg
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
-//padding
